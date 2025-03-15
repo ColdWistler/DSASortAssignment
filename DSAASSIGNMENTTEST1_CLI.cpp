30fcc0b486f1eb42be19655cf29d15f6da00482d
Soggy Genus <53271289+ColdWistler@users.noreply.github.com>
2025-03-15 16:53:27 +0545

    Update DSAASSIGNMENTTEST1_CLI.cpp

--- DSAASSIGNMENTTEST1_CLI.cpp
+++ DSAASSIGNMENTTEST1_CLI.cpp
@@ -65,38 +65,6 @@ void selectionSort(Node*& head) {
     } while (temp != head);
 }
 
-Node* linearSearch(Node* head, int key) {
-    if (head == nullptr) return nullptr;
-    Node* temp = head;
-    do {
-        if (temp->data.id == key) return temp;
-        temp = temp->next;
-    } while (temp != head);
-    return nullptr;
-}
-
-void deleteList(Node*& head) {
-    if (head == nullptr) return;
-    Node* temp = head;
-    do {
-        Node* nextNode = temp->next;
-        delete temp;
-        temp = nextNode;
-    } while (temp != head);
-    head = nullptr;
-}
-
-void generateRandomData(Node*& head, int N) {
-    srand(time(0));
-    for (int i = 0; i < N; i++) {
-        ComplexData data;
-        data.id = rand() % 1000;
-        data.name = "Item" + to_string(data.id);
-        data.value = (double)(rand() % 10000) / 100;
-        insert(head, data);
-    }
-}
-
 long long measureTime(void (*func)(Node*&), Node*& head) {
     auto start = high_resolution_clock::now();
     func(head);
@@ -106,15 +74,19 @@ long long measureTime(void (*func)(Node*&), Node*& head) {
 
 void analyzeAlgorithms(Node*& head, int N) {
     cout << "\nAlgorithm Performance Analysis" << endl;
-    cout << "--------------------------------------" << endl;
-    cout << setw(20) << left << "Algorithm" << setw(15) << "Time (ms)" << endl;
-    cout << "--------------------------------------" << endl;
+    cout << "----------------------------------------------------" << endl;
+    cout << setw(25) << left << "Algorithm" << setw(15) << "Time (ms)" << endl;
+    cout << "----------------------------------------------------" << endl;
     
-    long long selectionSortTime = measureTime(selectionSort, head);
-    cout << setw(20) << left << "Selection Sort" << setw(15) << selectionSortTime << endl;
+    Node* tempHead = head;
+    long long selectionSortTime = measureTime(selectionSort, tempHead);
+    cout << setw(25) << left << "Selection Sort" << setw(15) << selectionSortTime << endl;
     
     cout << "\nTime Complexity Analysis:" << endl;
     cout << "Selection Sort: Best - O(N^2), Average - O(N^2), Worst - O(N^2)" << endl;
+    cout << "Merge Sort: Best - O(N log N), Average - O(N log N), Worst - O(N log N)" << endl;
+    cout << "Binary Search: Best - O(1), Average - O(log N), Worst - O(log N)" << endl;
+    cout << "Interpolation Search: Best - O(1), Average - O(log log N), Worst - O(N)" << endl;
 }
 
 int main() {
@@ -126,15 +98,6 @@ int main() {
     cout << "Generated List: " << endl;
     printList(head);
     analyzeAlgorithms(head, N);
-    int key;
-    cout << "Enter the ID to search: ";
-    cin >> key;
-    Node* result = linearSearch(head, key);
-    if (result != nullptr) {
-        cout << "Element found: ID: " << result->data.id << ", Name: " << result->data.name << ", Value: " << result->data.value << endl;
-    } else {
-        cout << "Element not found" << endl;
-    }
     deleteList(head);
     return 0;
 }
